bca3f4523d27f85d7bc7e4b9368388b47ca0493b
Adam Varsányi <adam.varsanyi@riganti.cz>
2026-03-20 15:19:20 +0100

    Added ArchitectureTests

---
+++ CookBook.Clean.ArchitectureTests/ArchitectureTestBase.cs
@@ -0,0 +1,27 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using Assembly = System.Reflection.Assembly;
+
+namespace CookBook.Clean.ArchitectureTests;
+
+public class ArchitectureTestBase
+{
+    protected const string CoreNamespace = "CookBook.Clean.Core";
+    protected const string ApplicationNamespace = "CookBook.Clean.Application";
+    protected const string InfrastructureNamespace = "CookBook.Clean.Infrastructure";
+    protected const string MauiNamespace = "CookBook.Clean.Ui";
+    protected const string WebApiNamespace = "CookBook.Clean.WebApi";
+    
+    protected static readonly Assembly CoreAssembly = typeof(Core.IRootEntity).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(Application.Installer).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(Infrastructure.Installer).Assembly;
+    protected static readonly Assembly WebApiAssembly = typeof(Program).Assembly;
+
+    protected static readonly Architecture Architecture = new ArchLoader()
+        .LoadAssemblies(
+            CoreAssembly,
+            ApplicationAssembly,
+            InfrastructureAssembly,
+            WebApiAssembly)
+        .Build();
+}
