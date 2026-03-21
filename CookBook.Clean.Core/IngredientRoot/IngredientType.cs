085098b5eb05402b9abd159896768dcf1a3360bd
Adam Varsányi <adam.varsanyi@riganti.cz>
2026-03-21 22:39:21 +0100

    Added IngredientType enum

---
+++ CookBook.Clean.Core/IngredientRoot/IngredientType.cs
@@ -0,0 +1,84 @@
+﻿namespace CookBook.Clean.Core.IngredientRoot;
+
+public enum IngredientType
+{
+    // Fresh Produce
+    Vegetable,
+    Fruit,
+    Herb,
+    
+    // Proteins
+    Meat,
+    Fish,
+    Seafood,
+    Egg,
+    Legume,
+    Nut,
+    Seed,
+    
+    // Dairy & Alternatives
+    Milk,
+    Cream,
+    Butter,
+    Cheese,
+    Yogurt,
+    PlantMilk,
+    
+    // Grains & Baking Base
+    Flour,
+    Grain,
+    Oats,
+    Starch,
+    
+    // Sweeteners
+    Sugar,
+    BrownSugar,
+    PowderedSugar,
+    Honey,
+    Syrup,
+    Sweetener,
+    
+    // Chocolate & Flavor Base
+    Chocolate,
+    Cocoa,
+    Vanilla,
+    FlavorExtract,
+    
+    // Spices & Seasoning
+    Spice,
+    Salt,
+    
+    // Liquids & Drink Bases
+    Water,
+    MineralWater,
+    JuiceBase,
+    SoftDrink,
+    SyrupConcentrate,
+    
+    // Coffee & Tea (raw forms)
+    CoffeeBeans,
+    GroundCoffee,
+    TeaLeaves,
+    
+    // Alcohol (for recipes, not serving units)
+    Alcohol,
+    Wine,
+    Beer,
+    Spirit,
+    Liqueur,
+    
+    // Dessert & Ice Cream Ingredients
+    IceCreamBase,
+    Stabilizer,
+    Thickener,
+    Gelatin,
+    
+    // Additives & Extras
+    Oil,
+    Vinegar,
+    SauceBase,
+    ToppingIngredient,
+    
+    // Misc
+    Other
+}
