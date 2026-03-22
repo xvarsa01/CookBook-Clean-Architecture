b45699dcc5e5ad0f75d20c684234733ff28ff23c
Adam Varsányi <adam.varsanyi@riganti.cz>
2026-03-22 19:10:44 +0100

    Added validation for empty string in name of IngredientEntity

--- CookBook.Clean.Core/IngredientRoot/IngredientEntity.cs
+++ CookBook.Clean.Core/IngredientRoot/IngredientEntity.cs
@@ -1,14 +1,30 @@
 ﻿namespace CookBook.Clean.Core.IngredientRoot;
 
-public class IngredientEntity(string name, string? description, string? imageUrl) : IRootEntity
+// business rules:
+// - name can not be empty string
+
+public class IngredientEntity : IRootEntity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string Name { get; private set; } = name;
-    public string? Description { get; private set; } = description;
-    public string? ImageUrl { get; private set; } = imageUrl;
+    public string Name { get; private set; }
+    public string? Description { get; private set; }
+    public string? ImageUrl { get; private set; }
+
+    public IngredientEntity(string name, string? description, string? imageUrl)
+    {
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentNullException(nameof(name));
+        
+        Name =  name;
+        Description = description;
+        ImageUrl = imageUrl;
+    }
     
     public void UpdateName(string newName)
     {
+        if (string.IsNullOrEmpty(newName))
+            throw new ArgumentNullException(nameof(newName));
+        
         if (Name == newName) return;
         Name = newName;
     }
