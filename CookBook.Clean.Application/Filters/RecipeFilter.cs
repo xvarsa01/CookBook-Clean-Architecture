4bfc6a3d5d733689fc027da2d071685f71110d12
Adam Varsányi <adam.varsanyi@riganti.cz>
2026-03-23 12:33:52 +0100

    Added sort parameter enums and sorting by CreatedAt and ModifiedAt

--- CookBook.Clean.Application/Filters/RecipeFilter.cs
+++ CookBook.Clean.Application/Filters/RecipeFilter.cs
@@ -3,12 +3,21 @@ using CookBook.Clean.Core.RecipeRoot.Enums;
 
 namespace CookBook.Clean.Application.Filters;
 
-public class RecipeFilter : IFilter
+public class RecipeFilter : IFilter<RecipeSortParameter>
 {
     public string? Name { get; set; }
     public RecipeType? RecipeType { get; set; }
     public TimeSpan? MinimalDuration { get; set; }
     public TimeSpan? MaximalDuration { get; set; }
-    public string? SortParameterName { get; set; }
+    public RecipeSortParameter SortParameter { get; set; }
     public bool IsSortAscending { get; set; } = true;
+}
+
+public enum RecipeSortParameter
+{
+    Name,
+    Type,
+    Duration,
+    CreatedAt,
+    ModifiedAt
 }
