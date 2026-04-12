198247fee66d565af1f2ea0251a39f409fad0356
Adam Varsányi <adam.varsanyi@riganti.cz>
2026-04-12 21:36:21 +0200

    Registered start of outbox processor from MAUI app

--- src/CookBook.CleanArch.Presentation.MauiApplication/App.xaml.cs
+++ src/CookBook.CleanArch.Presentation.MauiApplication/App.xaml.cs
@@ -1,20 +1,33 @@
 ﻿using CookBook.CleanArch.Presentation.MauiApplication.Shells;
+using Microsoft.Extensions.Hosting;
 
 namespace CookBook.CleanArch.Presentation.MauiApplication;
 
 public partial class App
 {
     private readonly IServiceProvider _serviceProvider;
+    private readonly IReadOnlyCollection<IHostedService> _hostedServices;
     
     public App(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
+        _hostedServices = _serviceProvider.GetServices<IHostedService>().ToArray();
+        
         InitializeComponent();
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
+        _ = StartHostedServicesAsync(); // fire and forget, log exceptions inside
         var shell = _serviceProvider.GetRequiredService<AppShell>();
         return new Window(shell);
+    }
+    
+    private async Task StartHostedServicesAsync()
+    {
+        foreach (var hostedService in _hostedServices)
+        {
+            await hostedService.StartAsync(CancellationToken.None);
+        }
     }
 }
