5ad8f154a9ec1ef9943c861897233f7ee37f12d1
Adam Varsányi <adam.varsanyi@riganti.cz>
2026-04-14 13:45:30 +0200

    Removed Anroid and iOS platforms from MAUI

--- src/CookBook.CleanArch.Presentation.MauiApplication/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CookBook.CleanArch.Presentation.MauiApplication;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
