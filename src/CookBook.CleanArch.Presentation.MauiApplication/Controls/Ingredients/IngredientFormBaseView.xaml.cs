e607c1b9f106957d94ec1b39bb5faf7262a52ab2
Adam Varsányi <adam.varsanyi@riganti.cz>
2026-04-24 00:24:19 +0200

    Renamed component to FormBase

--- src/CookBook.CleanArch.Presentation.MauiApplication/Controls/Ingredients/IngredientFormBaseView.xaml.cs
+++ src/CookBook.CleanArch.Presentation.MauiApplication/Controls/Ingredients/IngredientFormBaseView.xaml.cs
@@ -3,16 +3,16 @@ using CookBook.CleanArch.Presentation.MauiApplication.ViewModels;
 
 namespace CookBook.CleanArch.Presentation.MauiApplication.Controls.Ingredients;
 
-public partial class IngredientCreateEditBaseView
+public partial class IngredientFormBaseView
 {
-    public IngredientCreateEditBaseView()
+    public IngredientFormBaseView()
     {
         InitializeComponent();
     }
     
     // INGREDIENT MODEL
     public static readonly BindableProperty IngredientProperty =
-        BindableProperty.Create(nameof(Ingredient), typeof(IngredientFormModel), typeof(IngredientCreateEditBaseView), default(IngredientFormModel), BindingMode.TwoWay);
+        BindableProperty.Create(nameof(Ingredient), typeof(IngredientFormModel), typeof(IngredientFormBaseView), default(IngredientFormModel), BindingMode.TwoWay);
     public IngredientFormModel Ingredient
     {
         get => (IngredientFormModel)GetValue(IngredientProperty);
@@ -21,7 +21,7 @@ public partial class IngredientCreateEditBaseView
 
     // SAVE COMMAND
     public static readonly BindableProperty SaveCommandProperty =
-        BindableProperty.Create(nameof(SaveCommand), typeof(ICommand), typeof(IngredientCreateEditBaseView));
+        BindableProperty.Create(nameof(SaveCommand), typeof(ICommand), typeof(IngredientFormBaseView));
     public ICommand SaveCommand
     {
         get => (ICommand)GetValue(SaveCommandProperty);
@@ -30,7 +30,7 @@ public partial class IngredientCreateEditBaseView
 
     // VALIDATE PROPERTY COMMAND
     public static readonly BindableProperty ValidatePropertyCommandProperty =
-        BindableProperty.Create(nameof(ValidatePropertyCommand), typeof(ICommand), typeof(IngredientCreateEditBaseView));
+        BindableProperty.Create(nameof(ValidatePropertyCommand), typeof(ICommand), typeof(IngredientFormBaseView));
     public ICommand ValidatePropertyCommand
     {
         get => (ICommand)GetValue(ValidatePropertyCommandProperty);
