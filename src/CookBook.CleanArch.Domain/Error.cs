e73112fd0aa179d40422f04bd02643966e5b39a4
Adam Varsányi <adam.varsanyi@riganti.cz>
2026-05-07 14:05:31 +0200

    Removed Arguments[] from equality comparation of Error to make tests pass

--- src/CookBook.CleanArch.Domain/Error.cs
+++ src/CookBook.CleanArch.Domain/Error.cs
@@ -1,6 +1,30 @@
-﻿namespace CookBook.CleanArch.Domain;
+﻿using System.Diagnostics.CodeAnalysis;
 
-public record Error(string Code, string Message, params object[] Arguments)
+namespace CookBook.CleanArch.Domain;
+
+public sealed record Error
 {
+    public required string Code { get; init; }
+    public required string Message { get; init; }
+    public object[] Arguments { get; init; } = [];
+
+    [SetsRequiredMembers]
+    public Error(string code, string message, params object[] arguments)
+    {
+        Code = code;
+        Message = message;
+        Arguments = arguments;
+    }
+
     public static readonly Error NullValue = new("Error.NullValue", "The specific result value is null.");
+    
+    public bool Equals(Error? other)
+        => other is not null
+           && Code == other.Code
+           && Message == other.Message;
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Code, Message);
+    }
 }
